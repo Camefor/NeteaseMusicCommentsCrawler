1240abd44b3c13856a9c644a13d14a3712e95dc9
ryhswang <xuehaq@gmail.com>
2021-09-26 17:30:57 +0800

    添加 业务入口类

---
+++ src/CommentsCrawlerService/Modules/MainService.cs
@@ -0,0 +1,35 @@
+﻿using CommentsCrawlerService.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommentsCrawlerService.Modules
+{
+    /// <summary>
+    /// 业务入口
+    /// </summary> 
+    public class MainService
+    {
+
+        private readonly UserPlayListService _userPlayListService = new UserPlayListService();
+        private readonly UserPlayDetailService _userPlayDetailService = new UserPlayDetailService();
+        private readonly MusicCommentsService _musicCommentsService = new MusicCommentsService();
+
+
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="atargetUserId">要查询的用户id</param>
+        public MainService(string atargetUserId)
+        {
+            GlobalStateData.uid = atargetUserId;
+        }
+
+
+
+
+    }
+}
