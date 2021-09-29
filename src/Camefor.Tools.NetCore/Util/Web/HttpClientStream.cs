a775714b8775dac8b21e5393749064cf3678be9d
ryhswang <xuehaq@gmail.com>
2021-09-29 10:18:50 +0800

    工具库中新增HttpClientUseStream

---
+++ src/Camefor.Tools.NetCore/Util/Web/HttpClientStream.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Camefor.Tools.NetCore.Util.Web
+{
+    /// <summary>
+    /// 描   述  ：                          
+    /// 版   本  ： V1.0.0                            
+    /// 创 建 人 ： rhyswang                                  
+    /// 日    期 ：                         
+    /// 创 建 人 ：                                   
+    /// 创建时间 ：                                  
+    /// 修 改 人 ：                                   
+    /// 修改时间 ：                                   
+    /// 修改描述 ：                                   
+    /// </summary> 
+    public class HttpClientStream
+    {
+        private static HttpClient _httpClient = new HttpClient();
+        private readonly JsonSerializerOptions _options;
+
+        public HttpClientStream()
+        {
+            _httpClient.Timeout = new TimeSpan(0, 0, 30);
+            _httpClient.DefaultRequestHeaders.Clear();
+            _options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        }
+
+        public async Task<T> GetObjectWithStream<T>(string url)
+        {
+            using (var response = await _httpClient.GetAsync(url, HttpCompletionOption.ResponseHeadersRead))
+            {
+                response.EnsureSuccessStatusCode();
+                var stream = await response.Content.ReadAsStreamAsync();
+                return await JsonSerializer.DeserializeAsync<T>(stream, _options);
+            }
+        }
+
+        public async Task<T> PostWithStream<T>(string url, Dictionary<string, string> dict, Dictionary<string, string> cookies = null)
+        {
+            if (cookies != null)
+            {
+                _httpClient = CreateHttpClient(url, cookies);
+            }
+            var request = new HttpRequestMessage(HttpMethod.Post, url) { Content = new FormUrlEncodedContent(dict) };
+            var response = await _httpClient.SendAsync(request);
+            var stream = await response.Content.ReadAsStreamAsync();
+            return new StreamReader(stream).ReadToEnd().ToObject<T>();
+        }
+
+
+
+        /// <summary>
+        /// 创建HttpClient
+        /// </summary>
+        /// <returns></returns>
+        public static HttpClient CreateHttpClient(string url, IDictionary<string, string> cookies = null)
+        {
+            //HttpClient httpclient;
+            HttpClientHandler handler = new HttpClientHandler();
+            var uri = new Uri(url);
+            if (cookies != null)
+            {
+                foreach (var key in cookies.Keys)
+                {
+                    string one = key + "=" + cookies[key];
+                    handler.CookieContainer.SetCookies(uri, one);
+                }
+            }
+            //如果是发送HTTPS请求  
+            if (url.StartsWith("https", StringComparison.OrdinalIgnoreCase))
+            {
+                ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+                _httpClient = new HttpClient(handler);
+            }
+            else
+            {
+                _httpClient = new HttpClient(handler);
+            }
+            return _httpClient;
+        }
+
+    }
+}
--- src/Camefor.Tools.NetCore/Util/Web/HttpMethods.cs
+++ src/Camefor.Tools.NetCore/Util/Web/HttpMethods.cs
@@ -35,8 +35,8 @@ namespace Camefor.Tools.NetCore.Util.Web
             HttpClient httpClient = CreateHttpClient(url);
             Task<string> result = httpClient.GetAsync(url).Result.Content.ReadAsStringAsync();
             return result.Result;
-        }   
-        public static string Get(string url,TimeSpan timeOut)
+        }
+        public static string Get(string url, TimeSpan timeOut)
         {
             HttpClient httpClient = CreateHttpClient(url);
             httpClient.Timeout = timeOut;
@@ -44,6 +44,12 @@ namespace Camefor.Tools.NetCore.Util.Web
             return result.Result;
         }
 
+        public static string Get(string url, Dictionary<string, string> cookies)
+        {
+            HttpClient httpClient = CreateHttpClient(url, cookies);
+            Task<string> result = httpClient.GetAsync(url).Result.Content.ReadAsStringAsync();
+            return result.Result;
+        }
         #endregion 
 
         #region POST
@@ -109,12 +115,12 @@ namespace Camefor.Tools.NetCore.Util.Web
             return await res.Content.ReadAsStringAsync();
         }
 
-        public static string Post(string url, Dictionary<string, string> dict)
+        public static string Post(string url, Dictionary<string, string> dict, Dictionary<string, string> cookies = null)
         {
             //var dict = new Dictionary<string, string>();
             //dict.Add("Input1", "TEST2");
             //dict.Add("Input2", "TEST2");
-            HttpClient client = CreateHttpClient(url);
+            HttpClient client = CreateHttpClient(url, cookies);
             var req = new HttpRequestMessage(HttpMethod.Post, url) { Content = new FormUrlEncodedContent(dict) };
             var res = client.Send(req);
             return res.Content.ReadAsStringAsync().Result;
@@ -138,26 +144,6 @@ namespace Camefor.Tools.NetCore.Util.Web
             return await res.Content.ReadAsStringAsync();
         }
 
-        [Obsolete]
-        public static string PostUseRestClient(string url)
-        {
-            var client = new RestClient(url);
-            client.Timeout = -1;
-            var request = new RestRequest(Method.POST);
-            request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
-            //request.AddHeader("Cookie", "NMTID=00OHpTtwZ-6TOCCLUyVi4AxrT2T5JwAAAF8DYR0GQ");
-            List<Parameter> parameters = new List<Parameter>();
-            parameters.Add(
-                new Parameter("params",
-                "fcP2s8qNzXjCEHXKk8MaBQP9TmZac4qyvKg1BbQ0qWATkIDPQMUXTviDT7TzkGMcoOhO4lfQAxCB88UHl5fCML3tLQY/xSXkUPeVpkFnkm2rNXrPfEFkYVqflUqgEZXlIWTmnFb7OfO1JdhFaUCFMeNfKz3DRV3DEyyuS9sL+zSazDzc1AA3fKLIKOJHSRrW"
-                    , ParameterType.RequestBody)
-                );
-
-            request.AddParameter("params", "fcP2s8qNzXjCEHXKk8MaBQP9TmZac4qyvKg1BbQ0qWATkIDPQMUXTviDT7TzkGMcoOhO4lfQAxCB88UHl5fCML3tLQY/xSXkUPeVpkFnkm2rNXrPfEFkYVqflUqgEZXlIWTmnFb7OfO1JdhFaUCFMeNfKz3DRV3DEyyuS9sL+zSazDzc1AA3fKLIKOJHSRrW");
-            request.AddParameter("encSecKey", "ab8af6344334422b3640a45234ebd29e94f390109059dc4aafe71783d842e1941ba4fd9f17af37a5dd92dbc5b5d465b0afcfe379017446ed70ffa144b1e9528353fb240c0eba309ace16486a9ec7fb7afce900725c9360e96d080cbf4459a5e027ec8e8834674c5dd9ddcde111ed8a2faf2c9f1d8069c7a81507caf069665b95");
-            IRestResponse response = client.Execute(request);
-            return response.Content;
-        }
 
 
         #endregion
